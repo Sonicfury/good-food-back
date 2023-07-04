63effdd2ba7d7b84c8e49b00480d5e69f0b88741
jbloup <jbloup@hotmail.fr>
2023-07-04 15:45:29 +0200

    fix(test): fll tests

---
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'nullable|string|email|unique:users',
+            'firstname' => 'nullable|string|min:1|max:25',
+            'lastname' => 'nullable|string|min:1|max:25',
+            'phone' => ['nullable', 'regex:/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/'],
+            'birthDate' => 'nullable|date',
+            'restaurant_id' => 'nullable|numeric',
+            'role' => 'nullable|string'
+        ];
+    }
+}
